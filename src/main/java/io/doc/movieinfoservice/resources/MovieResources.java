7d768373596cc1891f35ad8d2650cea7f418caef
Dupinder <dupinder.dhiman@catalystone.com>
2020-04-24 02:41:25 +0530

    initial commit

---
+++ src/main/java/io/doc/movieinfoservice/resources/MovieResources.java
@@ -0,0 +1,16 @@
+package io.doc.movieinfoservice.resources;
+
+import io.doc.movieinfoservice.models.Movie;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResources {
+
+    @RequestMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId){
+        return new Movie(movieId, "Inception");
+    }
+}
